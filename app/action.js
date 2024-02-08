0944363aa7b0cdbbb6fe80ac000ecf73725b0c97
srushti <srushti20@navgurukul.org>
2024-02-08 12:56:08 +0530

    add topnews row

--- app/action.js
+++ app/action.js
@@ -13,52 +13,82 @@ const Grid = () => {
         <Search/>
         </div>
       </div>
+      <div className='newrow'>
+        <p>TOP NEWS FROM INDIA</p>
+      </div>
       <div className="container">
         <div className="row">
           <div className="col-12 col-lg-4 box">
             <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+                <p>Read full article</p>
+              </a>
+            </div>
+          <div className="col-12 col-lg-4 box">
+          <img src="/cricket.png" alt="png"></img>
+            <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
+            <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+            <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+             <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
           </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
-          </div>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
+            </div>
           <div className="col-12 col-lg-4 box">
           <img src="/cricket.png" alt="png"></img>
             <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
             <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
-          </div>
-          <div className="col-12 col-lg-4 box">
-          <img src="/cricket.png" alt="png"></img>
-            <h1>Echoes of Defeat: A Personal Reflection on India’s 2023 World Cup Loss</h1>
-            <p>The first one read, “India's loss in the 2023 World Cup final has echoes of West Indies' defeat in the 1983 World Cup final.” The parallels are .....</p>
+            <a href="https://medium.com/@gouravmodi/echoes-of-defeat-a-personal-reflection-on-indias-2023-world-cup-loss-3177e485c1a0">
+              <p>Read full article</p>
+            </a>
           </div>
         </div>
       </div>
